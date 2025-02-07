1f3da624b8dd302c0486b4eae5b2bc0d34e733ac
Wayne Nin <wayne@insolutionsoftware.com>
2025-02-07 15:17:19 +0000

    add clarity intergration to dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,9 +2,13 @@
 
 import { DashboardPage } from "@/components/app-dashboard-page";
 import { AuthProvider } from '@/components/AuthContext';
-
+import Clarity from '@microsoft/clarity';
 
 export default function Home() {
+  const projectId = "q64g9fnhvi"
+
+  Clarity.init(projectId);
+
   return (
     
         <main className="flex-1 overflow-y-auto">
